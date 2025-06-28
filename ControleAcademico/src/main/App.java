5e79aeea4c212b644dda9fb2e107e62fc7032b8b
DanMulot <danilomulotto@hotmail.com>
2025-06-28 11:42:53 -0300

    iniciando o modelo professor

--- ControleAcademico/src/main/App.java
+++ ControleAcademico/src/main/App.java
@@ -1,19 +1,11 @@
 package main;
 
-import model.Professor;
+import ui.CadastroProfessorGui;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Professor professor = new Professor();
-        professor.setNome("Luis");
-        professor.setEndereco("Rua Pantaleao");
-        professor.setIdade(39);
-        professor.setMatricola(003);
-        professor.setCPF("24808747708");
-        professor.setAtuacao("tecnologia");
-        
-        
+     CadastroProfessorGui.executar();
     }
 }
         
--- ControleAcademico/src/model/Professor.java
+++ ControleAcademico/src/model/Professor.java
@@ -4,50 +4,83 @@ public class Professor {
     private String nome;
     private String endereco;
     private int idade;
-    private int matricola;
+    private int matricula;
     private String CPF;
     private String atuacao;
+
+    public Professor() {
+    }
+
+    public Professor(String nome, String endereco, int idade, int matricula, String cPF, String atuacao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.idade = idade;
+        this.matricula = matricula;
+        CPF = cPF;
+        this.atuacao = atuacao;
+    }
+
     public String getNome() {
         return nome;
     }
+
     public String getEndereco() {
         return endereco;
     }
+
     public int getIdade() {
         return idade;
     }
-    public int getMatricola() {
-        return matricola;
+
+    public int getMatricula() {
+        return matricula;
     }
+
     public String getCPF() {
         return CPF;
     }
+
     public String getAtuacao() {
         return atuacao;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
-    public void setMatricola(int matricola) {
-        this.matricola = matricola;
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
     }
+
     public void setCPF(String cPF) {
         CPF = cPF;
     }
+
     public void setAtuacao(String atuacao) {
         this.atuacao = atuacao;
     }
+
     @Override
     public String toString() {
-        return "Professor [nome=" + nome + ", endereco=" + endereco + ", idade=" + idade + ", matricola=" + matricola
+        return "Professor [nome=" + nome + ", endereco=" + endereco + ", idade=" + idade + ", matricola=" + matricula
                 + ", CPF=" + CPF + ", atuacao=" + atuacao + "]";
     }
-        
-}
 
+    public void visualizar() {
+        System.out.println("nome=" + nome);
+        System.out.println("endereco=" + endereco);
+        System.out.println("idade=" + idade);
+        System.out.println("CPF" + CPF);
+        System.out.println("atuacao" + atuacao);
+        System.out.println("matricula=" + matricula);
+    }
+
+}
